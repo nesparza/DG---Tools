0e2ac9b2a6db9eb4898dcb86e46e38858d984adc
nesparza <noe.eon@gmail.com>
2010-07-27 10:56:08 -0700

    DGSimBaseCalc...I changed it to a object orientated approach. It still needs some robusting and maybe implementation of args to make it more user-firendly...It is still script based

--- DGSimBaseCalc.py
+++ DGSimBaseCalc.py
@@ -20,45 +20,54 @@ Lame material constants
 
 @author: Noe
 """
-debug = 1
-
 # Import Library Section
-import numpy as np			#this loads the numpy module as np
 # End Import Section
 
-# Define the SI base for the Sim parameters
-baseLength    = 10e-6    # Meters
-baseYoungsMod = 1.75e6   # N/M^2
-nu            = 0.45     # poission ratio
-baseLJsigma   = 1e-9     # M
-baseLJepi     = 0.6e3     # J/mol - energy
+class DGParams:
+    scaledEMod = 1.0
+    scaledLJsigma = 1.0
+    scaledLJepi = 1.0
+    scaledLameMu = 1.0
+    scaledLameLambda = 1.0
+    
+    def __init__(self,baseLength = 10e-6,baseEMod = 1.75e6,baseLJsigma = 1e-9,
+    baseLJepi = 0.6e3, nu = 0.45):
+        self.baseLength = baseLength
+        self.baseEMod = baseEMod
+        self.baseLJsigma = baseLJsigma
+        self.baseLJepi = baseLJepi
+        self.nu = nu
+    
+    def scaleParams(self):
+        self.scaledEMod = self.baseEMod * self.baseLength**2.0
+        self.scaledLameMu = self.scaledEMod/(2*(1+self.nu))
+        self.scaledLameLambda = (self.scaledEMod*self.nu)/((1+self.nu)*(1-2*self.nu))
+        self.scaledLJsigma = self.baseLJsigma / self.baseLength
+        self.scaledLJepi = self.baseLJepi # energy doesnt need to be scaled?
+    
+    def _Print(self):
+        string_base = "Base - Length: %.2e EMod: %.2e LJSigma: %.2e LJEpi: %2e"\
+        % (self.baseLength, self.baseEMod, self.baseLJsigma, self.baseLJepi)
+        string_scaled = "Scaled - Emod: %.2e LameMu: %.2e LameLambda: %.2e LJSigma: %.2e LJEpi: %.2e" \
+        % (self.scaledEMod, self.scaledLameMu, self.scaledLameLambda, self.scaledLJsigma, self.scaledLJepi)
+        
+        return "%s \n%s" % (string_base, string_scaled)
+
+    def Print(self):
+        print self._Print()
+        
+
 
 # BEGIN main code
+def main():
+    debug = 1
+    simNewBase= DGParams()
+    simNewBase.scaleParams()
 
-# Calculate scaled Lame Material Constants
-if debug == 1:
-    # SI base lame constants
-    baseLameMu = baseYoungsMod/(2*(1+nu))
-    baseLameLambda = (baseYoungsMod*nu)/((1+nu)*(1-2*nu))
-    
-    print "%s %0.2e\n"*2 % ("base Mu:", baseLameMu, "base Lambda:", 
-    baseLameLambda)
+    if debug == 1:
+        simNewBase.Print()
 
-# Scale Young's Modulus
-scaledYoungsMod = baseYoungsMod * baseLength**2.0
+if __name__ == '__main__':
+    main()
 
-# Scale Lame Constants
-scaledLameMu = scaledYoungsMod/(2*(1+nu))
-scaledLameLambda = (scaledYoungsMod*nu)/((1+nu)*(1-2*nu))
-
-if debug == 1:
-    print "%s %0.2e\n"*3 % ("Scaled E:", scaledYoungsMod,
-    "Scaled Mu",scaledLameMu,"Scaled Lambda",scaledLameLambda)
-
-# Scale LJ Constants
-scaledLJsigma = baseLJsigma / baseLength
-scaledLJepi = baseLJepi # energy doesnt need to be scaled?
-
-if debug == 1:
-    print "%s %0.2e\n"*2 % ("Scaled LJ Sigma:",scaledLJsigma,
-    "Scaled LJ Epislon:",scaledLJepi)
+# END main code
